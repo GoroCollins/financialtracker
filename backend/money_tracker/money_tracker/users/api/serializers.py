5cfa298a818feb282bca2b8af13689efa31ee120
Goro Collins <gorocollins@gmail.com>
2025-03-23 20:59:58 +0300

    Removed google signup via django-rest-auth

--- backend/money_tracker/money_tracker/users/api/serializers.py
+++ backend/money_tracker/money_tracker/users/api/serializers.py
@@ -1,8 +1,5 @@
 from rest_framework import serializers
 from dj_rest_auth.serializers import LoginSerializer, TokenSerializer, UserDetailsSerializer
-from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
-from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-# from dj_rest_auth.registration.views import SocialLoginView
 from money_tracker.users.models import User
 
 
@@ -29,14 +26,3 @@ class CustomTokenSerializer(TokenSerializer):
     class Meta:
         model = User
         fields = ('key', 'user')  # Include the user details in the login response
-        
-
-# def get_social_login_view():
-#     """Lazy import to avoid circular import issues."""
-#     from dj_rest_auth.registration.views import SocialLoginView # type: ignore
-#     return SocialLoginView
-
-# class GoogleLogin(get_social_login_view()):  # Import SocialLoginView dynamically
-#     adapter_class = GoogleOAuth2Adapter
-#     callback_url = "http://localhost:8000/accounts/google/login/callback/"
-#     client_class = OAuth2Client
