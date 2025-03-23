6d2652b6abdd488affd39fc4aa90c968c65260d6
Goro Collins <gorocollins@gmail.com>
2025-03-23 13:23:14 +0300

    Removed the credentials

--- backend/money_tracker/money_tracker/users/api/serializers.py
+++ backend/money_tracker/money_tracker/users/api/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
-
+from dj_rest_auth.serializers import LoginSerializer, TokenSerializer, UserDetailsSerializer
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+# from dj_rest_auth.registration.views import SocialLoginView
 from money_tracker.users.models import User
 
 
@@ -11,3 +14,29 @@ class UserSerializer(serializers.ModelSerializer[User]):
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "username"},
         }
+
+class CustomUserDetailsSerializer(UserDetailsSerializer):
+    #profile_image = serializers.ImageField(read_only=True)
+    name = serializers.CharField()
+
+    class Meta:
+        model = UserDetailsSerializer.Meta.model
+        fields = UserDetailsSerializer.Meta.fields + ('name',)
+
+class CustomTokenSerializer(TokenSerializer):
+    user = CustomUserDetailsSerializer(read_only=True)  # Use your custom user serializer
+
+    class Meta:
+        model = User
+        fields = ('key', 'user')  # Include the user details in the login response
+        
+
+# def get_social_login_view():
+#     """Lazy import to avoid circular import issues."""
+#     from dj_rest_auth.registration.views import SocialLoginView # type: ignore
+#     return SocialLoginView
+
+# class GoogleLogin(get_social_login_view()):  # Import SocialLoginView dynamically
+#     adapter_class = GoogleOAuth2Adapter
+#     callback_url = "http://localhost:8000/accounts/google/login/callback/"
+#     client_class = OAuth2Client
