016d77688ad29facbf08d4425db6786ffa1e34f2
Goro Collins <gorocollins@gmail.com>
2025-04-27 20:54:22 +0300

    Added user profile image

--- backend/money_tracker/money_tracker/users/api/serializers.py
+++ backend/money_tracker/money_tracker/users/api/serializers.py
@@ -6,19 +6,19 @@ from money_tracker.users.models import User
 class UserSerializer(serializers.ModelSerializer[User]):
     class Meta:
         model = User
-        fields = ["username", "name", "url"]
+        fields = ["username", "name", "url", "profile_image"]
 
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "username"},
         }
 
 class CustomUserDetailsSerializer(UserDetailsSerializer):
-    #profile_image = serializers.ImageField(read_only=True)
+    profile_image = serializers.ImageField(read_only=True)
     name = serializers.CharField()
 
     class Meta:
         model = UserDetailsSerializer.Meta.model
-        fields = UserDetailsSerializer.Meta.fields + ('name',)
+        fields = UserDetailsSerializer.Meta.fields + ('profile_image','name',)
 
 class CustomTokenSerializer(TokenSerializer):
     user = CustomUserDetailsSerializer(read_only=True)  # Use your custom user serializer
--- backend/money_tracker/money_tracker/users/models.py
+++ backend/money_tracker/money_tracker/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, ImageField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -15,6 +15,7 @@ class User(AbstractUser):
     name = CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore[assignment]
     last_name = None  # type: ignore[assignment]
+    profile_image = ImageField(upload_to='uploads/images', null=True, blank=True)
 
     def get_absolute_url(self) -> str:
         """Get URL for user's detail view.
