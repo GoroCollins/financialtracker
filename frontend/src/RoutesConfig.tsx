3b23069caadb1d93895818e3ff7e04c9b7e8b515
Goro Collins <gorocollins@gmail.com>
2025-05-12 19:47:23 +0300

    Added login url

--- frontend/src/RoutesConfig.tsx
+++ frontend/src/RoutesConfig.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Signup from "./authentication/Signup";
 import PublicHomePage from "./PublicHome";
+import Login from "./authentication/Login";
 
 const RoutesConfig: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<PublicHomePage />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
