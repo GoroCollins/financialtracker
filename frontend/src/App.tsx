19d3ea9afef8a7eba746de1ea97ed29135fd4a84
Goro Collins <gorocollins@gmail.com>
2025-05-12 19:45:23 +0300

    Wrap up the app in auth provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
-import './App.css'
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from './authentication/AuthenticationService'; // Make sure to import AuthProvider
+import { BrowserRouter as Router } from 'react-router-dom';
 import RoutesConfig from "./RoutesConfig";
 
 function App() {
+
   return (
-    <BrowserRouter>
-      <RoutesConfig />
-    </BrowserRouter>
+    <Router>
+      <AuthProvider>  {/* Wrap your entire app inside AuthProvider */}
+        <RoutesConfig  />
+      </AuthProvider>
+    </Router>
   );
 }
 
 export default App;
+
