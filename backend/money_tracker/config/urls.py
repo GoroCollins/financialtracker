3e41524fae795f579a73592e0a177e4c03ddf59b
Goro Collins <gorocollins@gmail.com>
2025-06-07 16:43:08 +0300

    Align the url declarations

--- backend/money_tracker/config/urls.py
+++ backend/money_tracker/config/urls.py
@@ -38,11 +38,7 @@ urlpatterns += [
     # DRF auth token
     path("api/auth-token/", obtain_auth_token),
     path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
-    path(
-        "api/docs/",
-        SpectacularSwaggerView.as_view(url_name="api-schema"),
-        name="api-docs",
-    ),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="api-schema"), name="api-docs",),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
