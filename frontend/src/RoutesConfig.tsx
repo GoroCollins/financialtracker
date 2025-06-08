9d23cf81200675c094af79dd27b1eeaf4f1d4f45
Goro Collins <gorocollins@gmail.com>
2025-06-08 17:16:16 +0300

    Add protected route and efficient user authentication flow

--- frontend/src/RoutesConfig.tsx
+++ frontend/src/RoutesConfig.tsx
@@ -3,13 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import Signup from "./authentication/Signup";
 import PublicHomePage from "./PublicHome";
 import Login from "./authentication/Login";
-
+import VerifyEmailNotice from "./authentication/VerifyEmailNotice";
+import ConfirmEmail from "./authentication/ConfirmEmail";
+import Home from "./Home";
+import { ProtectedRoute } from "./ProtectedRoutes";
 const RoutesConfig: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<PublicHomePage />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/verify-email" element={<VerifyEmailNotice />} />
+      <Route path="/confirm-email/:key" element={<ConfirmEmail />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
     </Routes>
   );
 }
