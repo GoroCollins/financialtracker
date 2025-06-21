6db2eb5a64b8548f46788f3e5f253401f2fbcfdc
Goro Collins <gorocollins@gmail.com>
2025-06-21 20:37:16 +0300

    Integrated toast notifications to the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthProvider } from './authentication/AuthenticationService'; // Make sure to import AuthProvider
 import { BrowserRouter as Router } from 'react-router-dom';
 import RoutesConfig from "./RoutesConfig";
+import { Toaster } from 'react-hot-toast';
 
 function App() {
 
   return (
     <Router>
       <AuthProvider>  {/* Wrap your entire app inside AuthProvider */}
+        <Toaster position="top-right" />
         <RoutesConfig  />
       </AuthProvider>
     </Router>
