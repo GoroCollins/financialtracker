8b880fd904ee23e89cfedaa81e7526f89b46d787
Goro Collins <gorocollins@gmail.com>
2025-06-29 21:47:48 +0300

    Added style react library

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 import { ErrorInfo } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import ErrorPage from './GlobalErrorHandler.tsx';
+import "react-datepicker/dist/react-datepicker.css";
 
 // Custom error handler function to log error
 const errorHandler = (error: Error, info: ErrorInfo) => {
