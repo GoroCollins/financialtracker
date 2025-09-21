96e9a489e5a7f27113b9410b416a0f7876f5992c
Goro Collins <gorocollins@gmail.com>
2025-09-21 19:15:30 +0300

    Add new model fields to the serializer

--- backend/money_tracker/money_tracker/users/api/serializers.py
+++ backend/money_tracker/money_tracker/users/api/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from dj_rest_auth.serializers import LoginSerializer, TokenSerializer, UserDetailsSerializer
 from money_tracker.users.models import User
+from django.conf import settings
+from dj_rest_auth.registration.serializers import RegisterSerializer
 
 
 class UserSerializer(serializers.ModelSerializer[User]):
     full_name = serializers.CharField(read_only=True)
     class Meta:
         model = User
-        fields = ["username", "first_name", "middle_name", "last_name", "full_name", "url", "profile_image"]
+        fields = ["username", "first_name", "middle_name", "last_name", "full_name", "url", "profile_image", "phone_number"]
 
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "username"},
@@ -19,7 +21,7 @@ class CustomUserDetailsSerializer(UserDetailsSerializer):
 
     class Meta:
         model = UserDetailsSerializer.Meta.model
-        fields = UserDetailsSerializer.Meta.fields + ('profile_image','full_name', "middle_name",)
+        fields = UserDetailsSerializer.Meta.fields + ('profile_image','full_name', "middle_name", "phone_number")
 
 class CustomTokenSerializer(TokenSerializer):
     user = CustomUserDetailsSerializer(read_only=True)  # Use your custom user serializer
@@ -27,3 +29,54 @@ class CustomTokenSerializer(TokenSerializer):
     class Meta:
         model = User
         fields = ('key', 'user')  # Include the user details in the login response
+        
+
+# class CustomRegisterSerializer(RegisterSerializer):
+#     phone_number = serializers.CharField(required=False)
+#     middle_name = serializers.CharField(required=False)
+#     _has_phone_field = True  # 🛠️ Important for compatibility with allauth
+
+#     def get_cleaned_data(self):
+#         data = super().get_cleaned_data()
+#         data['phone_number'] = self.validated_data.get('phone_number', '')
+#         data['middle_name'] = self.validated_data.get('middle_name', '')
+#         return data
+
+#     def custom_signup(self, request, user):
+#         user.phone_number = self.validated_data.get('phone_number', '')
+#         user.middle_name = self.validated_data.get('middle_name', '')
+#         user.save()
+#         return user
+
+
+from allauth.account import app_settings as allauth_settings
+
+def is_required(field_name: str) -> bool:
+    """
+    Check if a field is marked as required using the '*' suffix convention
+    """
+    required_fields = allauth_settings.SIGNUP_FIELDS
+    return f"{field_name}*" in required_fields
+
+class CustomRegisterSerializer(RegisterSerializer):
+    username = serializers.CharField(required=is_required("username"))
+    email = serializers.EmailField(required=is_required("email"))
+    phone_number = serializers.CharField(required=is_required("phone_number"), allow_blank=True)
+    middle_name = serializers.CharField(required=is_required("middle_name"), allow_blank=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._has_phone_field = "phone_number" in settings.ACCOUNT_SIGNUP_FIELDS
+
+    def get_cleaned_data(self):
+        data = super().get_cleaned_data()
+        data['phone_number'] = self.validated_data.get('phone_number', '')
+        data['middle_name'] = self.validated_data.get('middle_name', '')
+        return data
+
+    def custom_signup(self, request, user):
+        user.phone_number = self.validated_data.get('phone_number', '')
+        user.middle_name = self.validated_data.get('middle_name', '')
+        user.save()
+        return user
+
