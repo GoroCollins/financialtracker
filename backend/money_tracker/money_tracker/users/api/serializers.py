06ed42ad832ea00dc35e44bd4b111d7308edadf2
Goro Collins <gorocollins@gmail.com>
2025-10-16 22:01:24 +0300

    Customize the password reset link

--- backend/money_tracker/money_tracker/users/api/serializers.py
+++ backend/money_tracker/money_tracker/users/api/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
-from dj_rest_auth.serializers import LoginSerializer, TokenSerializer, UserDetailsSerializer
+from dj_rest_auth.serializers import LoginSerializer, TokenSerializer, UserDetailsSerializer, PasswordResetSerializer, PasswordResetConfirmSerializer
 from money_tracker.users.models import User
 from django.conf import settings
 from dj_rest_auth.registration.serializers import RegisterSerializer
+from django.contrib.auth.forms import PasswordResetForm
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from allauth.account.forms import default_token_generator
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.translation import gettext_lazy as _
+from allauth.account.utils import user_pk_to_url_str as uid_encoder
+from allauth.account.utils import url_str_to_user_pk as uid_decoder
+
 
 
 class UserSerializer(serializers.ModelSerializer[User]):
@@ -79,4 +89,44 @@ class CustomRegisterSerializer(RegisterSerializer):
         user.middle_name = self.validated_data.get('middle_name', '')
         user.save()
         return user
+        
+class CustomPasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField()
 
+    def save(self, **kwargs):
+        request = self.context.get('request')
+        email = self.validated_data['email']
+        frontend_url = getattr(settings, "FRONTEND_URL", "http://localhost:5173")
+        expiry_minutes = getattr(settings, "PASSWORD_RESET_TIMEOUT", 259200) // 60
+
+        # Find all active users matching this email
+        users = User.objects.filter(email__iexact=email, is_active=True)
+        if not users.exists():
+            return  # silently ignore invalid emails
+
+        for user in users:
+            # Encode UID properly (base36 if allauth, base64 if django)
+            uid = uid_encoder(user)
+            token = default_token_generator.make_token(user)
+            reset_url = f"{frontend_url}/resetpassword/{uid}/{token}/"
+
+            context = {
+                "user": user,
+                "user_name": user.full_name,
+                "reset_url": reset_url,
+                "frontend_url": frontend_url,
+                "site_name": getattr(settings, "SITE_NAME", "My App"),
+                "expiry_minutes": expiry_minutes,
+            }
+
+            subject = "Password Reset Request"
+            html_message = render_to_string("djrestauth/custom_password_reset_email.html", context)
+            plain_message = f"You requested a password reset.\nReset your password: {reset_url}"
+
+            send_mail(
+                subject,
+                plain_message,
+                settings.DEFAULT_FROM_EMAIL,
+                [user.email],
+                html_message=html_message,
+            )
