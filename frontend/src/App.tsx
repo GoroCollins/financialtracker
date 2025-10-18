c0693a49bb0d0a3e818aae9c5d91074107015038
Goro Collins <gorocollins@gmail.com>
2025-10-18 10:37:02 +0300

    Wrap the app in shadcn theme provider and theme toggling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import RoutesConfig from "./RoutesConfig";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
         <AuthProvider>{/* Wrap your entire app inside AuthProvider */}
           <RoutesConfig />
           <Toaster position="top-right" />
+          <div className="fixed bottom-4 right-4">
+            <ModeToggle />
+          </div>
         </AuthProvider>
       </Router>
     </ThemeProvider>
