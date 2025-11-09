4c25eba959296dd2e7215472626e470d7d3d78d2
Goro Collins <gorocollins@gmail.com>
2025-11-09 16:37:53 +0300

    loading of types for the project

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,12 @@
+/// <reference types="vitest" />
 /// <reference types="vite/client" />
+
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_OTHER_KEY?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
